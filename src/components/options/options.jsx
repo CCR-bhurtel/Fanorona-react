fec61cfc1fb89b2172fe8423120abf3160086700
cceer <bhurtelshishir@gmail.com>
2020-10-19 16:40:12 +0545

    styles restart feature and give up feature upgraded

--- src/components/options/options.jsx
+++ src/components/options/options.jsx
@@ -11,9 +11,8 @@ class Options extends React.Component {
       <div id="buttons" className={cssObj.Options}>
         {started && <button className="btn waves-effect waves-light   red "   onClick={() => gameOver()}>Give up</button>}
         {combo && <button className="btn waves-effect waves-light green"  onClick={nextTurn}>Next turn</button>}
-        {!started && <button className="btn waves-effect waves-light blue lighten-3"  onClick={startGame}>Start!</button>}
-        {started &&  <button className="btn waves-effect waves-light blue" onClick={startGame} >
-         Restart</button>}
+        {!started && <button className="btn waves-effect waves-light "  onClick={startGame}>Start!</button>}
+        {started &&  <button className="btn waves-effect  blue" onClick={startGame} >Restart</button>}
       </div>
     )
   }
